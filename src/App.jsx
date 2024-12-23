61d060146f0c02b8a50fbb6c5ae66c52db165a4d
tony-42069 <dsadellari@abizadvisors.com>
2024-12-22 21:09:32 -0500

    Added README file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatBox from './components/Chat/ChatBox';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
